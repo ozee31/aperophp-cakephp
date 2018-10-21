a03762368ef82dad5b1b03343b228ef7705bc095
Ozee31 <flavien@commit42.fr>
2018-10-21 17:22:11 +0200

    install twig + bootstrap & update readme

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 
 Requis :
 
-- PHP >= 5.6
+- PHP >= 7.1
 - Composer
 - L'extension PHP mbstring
 - L'extension PHP intl
@@ -24,3 +24,21 @@ Requis :
 ```shell
 composer install
 ```
+
+## Commandes
+
+- Composer (docker) : `docker-compose run --rm composer --ignore-platform-reqs `
+
+- Cake Shell (docker) : `docker-compose run --rm web bin/cake`
+
+## Documentation
+
+### CakePHP
+
+- Book : https://book.cakephp.org/3.0/fr/index.html
+- Api : https://api.cakephp.org/3.6/
+
+### Plugins
+
+- CakePHP-Twig : https://github.com/WyriHaximus/TwigView
+- Bootstrap UI : https://github.com/FriendsOfCake/bootstrap-ui/tree/develop
--- src/View/AppView.php
+++ src/View/AppView.php
@@ -1,19 +1,7 @@
 <?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     3.0.0
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- */
 namespace App\View;
 
-use Cake\View\View;
+use WyriHaximus\TwigView\View\TwigView;
 
 /**
  * Application View
@@ -22,7 +10,7 @@ use Cake\View\View;
  *
  * @link https://book.cakephp.org/3.0/en/views.html#the-app-view
  */
-class AppView extends View
+class AppView extends TwigView
 {
 
     /**
@@ -36,5 +24,17 @@ class AppView extends View
      */
     public function initialize()
     {
+        parent::initialize();
+        $this->loadHelper('Html', ['className' => 'BootstrapUI.Html']);
+        $this->loadHelper('Form', ['className' => 'BootstrapUI.Form']);
+        $this->loadHelper('Breadcrumbs', ['className' => 'BootstrapUI.Breadcrumbs']);
+        $this->loadHelper('Flash', ['className' => 'BootstrapUI.Flash']);
+        $this->loadHelper('Number');
+        $this->loadHelper('Paginator', ['className' => 'BootstrapUI.Paginator']);
+        $this->loadHelper('Rss');
+        $this->loadHelper('Session');
+        $this->loadHelper('Text');
+        $this->loadHelper('Time');
+        $this->loadHelper('Url');
     }
 }
